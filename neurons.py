4f83657ac6a2615a9ab2112c384dc592270d7d07
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2026-09-13 16:55:24 -0300

    Added docstrings to module neurons

--- neurons.py
+++ neurons.py
@@ -1,7 +1,14 @@
+"""
+Defines a single neuron
+"""
+
 import numpy as np
 
 
 def single_neuron(inputs):
+    """
+    Implements a single neuron
+    """
     input_length = len(inputs)
 
     weights = np.random.rand(1, input_length)
